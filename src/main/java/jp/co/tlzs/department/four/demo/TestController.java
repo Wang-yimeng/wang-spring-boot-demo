9c35d601a9b1e54d1483d02eaae8c884118c5821
Wangyimeng <n727227318@gmail.com>
2020-08-04 23:23:37 +0900

    no message

--- src/main/java/jp/co/tlzs/department/four/demo/TestController.java
+++ src/main/java/jp/co/tlzs/department/four/demo/TestController.java
@@ -1,10 +1,12 @@
 package jp.co.tlzs.department.four.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+	@CrossOrigin
     @GetMapping("/test")
     public TestBean test() {
         return new TestBean("aaaa");
